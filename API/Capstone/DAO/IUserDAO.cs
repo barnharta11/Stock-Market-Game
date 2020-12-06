2e988d66f8e450009b15a7c70f55ae86b0a18277
Alicia Barnhart <aliciambarnhart@gmail.com>
2020-12-06 09:21:43 -0500

    idao update user

--- API/Capstone/DAO/IUserDAO.cs
+++ API/Capstone/DAO/IUserDAO.cs
@@ -6,6 +6,6 @@ namespace Capstone.DAO
     public interface IUserDAO
     {
         User GetUser(string username);
-        User AddUser(string username, string password, string role);
+        User AddUser(string username, string email, string password, string role);
     }
 }
