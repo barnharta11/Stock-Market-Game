cbbb6087108b31e16b08053f9b04ed2c0654f7e2
Alicia Barnhart <aliciambarnhart@gmail.com>
2020-12-09 17:23:35 -0500

    sql updates

--- API/database/capstone.sql
+++ API/database/capstone.sql
@@ -23,13 +23,13 @@ CREATE TABLE users (
 	salt varchar(200) NOT NULL,
 	user_role varchar(50) NOT NULL
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
-)
+);
 
 Create Table game_status (
 	status_id int NOT NULL,
 	status_name varchar(50) NOT NULL,
 	Constraint pk_status_id Primary Key (status_id),
-)
+);
 
 CREATE TABLE games (
 	game_id int IDENTITY(1,1) NOT NULL,
@@ -41,22 +41,67 @@ CREATE TABLE games (
 	Constraint fk_games_users_creator Foreign Key (creator_id) References users(user_id), 
 	--Constraint fk_games_users_winner Foreign Key (winner_id) References users (user_id),
 	constraint pk_games Primary Key (game_id)
-)
+);
+
+Create Table portfolio (
+	user_game_id int NOT NULL,
+	balance money NOT NULL,
+	portfolio_id int IDENTITY(1,1) NOT NULL,
+	Constraint pk_portfolio_portfolio_id Primary Key (portfolio_id),
+);	
 
 Create Table user_games (
 	user_id int NOT NULL,
 	game_id int NOT NULL,
-	balance int NOT NULL,
 	user_game_id int IDENTITY(1,1) NOT NULL,
 	status_code int NOT NULL,
+	portfolio_id int NOT NULL,
 	Constraint pk_user_game_id Primary Key (user_game_id),
 	Constraint fk_user_games_user_id Foreign Key (user_id) References users(user_id),
 	Constraint fk_user_games_game_id Foreign Key (game_id) References games (game_id),
 	Constraint fk_user_games_game_status Foreign Key (status_code) References game_status (status_id),
+	Constraint fk_user_games_portfolio Foreign Key (portfolio_id) References portfolio (portfolio_id),
 	Constraint ck_status_code check(status_code in (0,1))
-)
+);
 
+Create Table stocks (
+	stock_id int NOT NULL,
+	stock_ticker varchar(50) NOT NULL,
+	company_name varchar(50) NOT NULL,
+	stock_price money NOT NULL,
+	Constraint pk_stocks_stock_id Primary Key (stock_id),
+);
 
+Create Table portfolio_stocks (
+	portfolio_id int NOT NULL,
+	stock_id int NOT NULL,
+	quantity_held int NOT NULL,
+	portfolio_stocks_id int IDENTITY(1,1) NOT NULL,
+	Constraint pk_portfolio_stocks_portfolio_stocks_id Primary Key (portfolio_stocks_id),
+	Constraint fk_portfolio_stocks_portfolio Foreign Key (portfolio_id) References portfolio(portfolio_id),
+	Constraint fk_portfolio_stocks_stock Foreign Key (stock_id) References stocks(stock_id),
+);
+
+Create Table transaction_names (
+	trans_id int NOT NULL,
+	trans_name varchar(25) NOT NULL,
+	Constraint pk_transactions_names_trans_id Primary Key (trans_id),
+	
+);
+
+Create Table transactions (
+	transaction_id int IDENTITY(1,1) NOT NULL,
+	trans_type_id int NOT NULL,
+	price money NOT NULL,
+	time datetime  NOT NULL,
+	user_game_id int NOT NULL,
+	stock_id int NOT NULL,
+	Constraint pk_transaction_id Primary Key (transaction_id),
+	Constraint fk_transactions_stocks_user_games Foreign Key (user_game_id) References user_games(user_game_id),
+	Constraint fk_transactions_stocks_stocks Foreign Key (stock_id) References stocks(stock_id),
+	Constraint ck_trans_type_id check(trans_type_id in (0,1)),
+	Constraint fk_transactions_type_transaction_names Foreign Key (trans_type_id) References transaction_names(trans_id),
+);
 
 --populate default data
 INSERT INTO users (username, email, password_hash, salt, user_role) VALUES ('user', 'makesomethingup@someplace.com', 'Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
@@ -64,10 +109,13 @@ INSERT INTO users (username, email, password_hash, salt, user_role) VALUES ('adm
 
 INSERT INTO	game_status (status_id, status_name) VALUES (0, 'Pending'), (1, 'Accepted'), (2, 'Completed');
 
+INSERT INTO	transaction_names (trans_id, trans_name) VALUES (0, 'Buy'), (1, 'Sell');
+
 INSERT INTO games (game_name, creator_id, start_date, end_date) VALUEs ('Wall St. Wolves', 1, 12/07/2020, 12/16/2020);
 INSERT INTO games (game_name, creator_id, start_date, end_date) VALUEs ('Day Trading Daddies', 1, 12/08/2020, 12/17/2020);
 INSERT INTO games (game_name, creator_id, start_date, end_date) VALUEs ('Martha Stewart Exchange', 2, 12/09/2020, 12/18/2020);
 INSERT INTO games (game_name, creator_id, start_date, end_date) VALUEs ('Feds Keep Out', 2, 12/10/2020, 12/19/2020);
+INSERT INTO games (game_name, creator_id, start_date, end_date) VALUEs ('Nothing to SEC Here', 1, 12/11/2020, 12/20/2020);
 
 INSERT INTO user_games (user_id, game_id, balance, status_code) VALUEs (1, 1, 100000, 1);
 INSERT INTO user_games (user_id, game_id, balance, status_code) VALUEs (1, 2, 100000, 1);
@@ -75,6 +123,11 @@ INSERT INTO user_games (user_id, game_id, balance, status_code) VALUEs (1, 3, 10
 INSERT INTO user_games (user_id, game_id, balance, status_code) VALUEs (2, 3, 100000, 1);
 INSERT INTO user_games (user_id, game_id, balance, status_code) VALUEs (2, 4, 100000, 1);
 INSERT INTO user_games (user_id, game_id, balance, status_code) VALUEs (2, 1, 100000, 0);
+INSERT INTO user_games (user_id, game_id, balance, status_code) VALUEs (2, 5, 100000, 0);
+INSERT INTO user_games (user_id, game_id, balance, status_code) VALUEs (1, 5, 100000, 1);
 
+--table alterations
+
+Alter Table porfolio ADD Constraint fk_portfolio_user_games Foreign Key (user_game_id) References user_games(user_game_id)
 
 GO
