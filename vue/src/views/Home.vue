a7fc855575b3578cfa7abb24d25eefe8ef3f1e3d
David Perez <davidperez3611@gmail.com>
2020-12-09 09:38:31 -0500

    CSS styling

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -12,3 +12,9 @@ export default {
   name: "home"
 };
 </script>
+
+<style>
+
+
+</style>
+
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,51 +1,20 @@
 <template>
-  <div id="register" class="text-center">
+  <div id="register" class="container">
     <form class="form-register" @submit.prevent="register">
-      <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
+      <div id="header" class="maintext">Create Account</div>
       <div class="alert alert-danger" role="alert" v-if="registrationErrors">
         {{ registrationErrorMsg }}
       </div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="email" class="sr-only">Email</label>
-      <input
-        type="text"
-        id="email"
-        class="form-control"
-        placeholder="Email"
-        v-model="user.email"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <input
-        type="password"
-        id="confirmPassword"
-        class="form-control"
-        placeholder="Confirm Password"
-        v-model="user.confirmPassword"
-        required
-      />
-      <router-link :to="{ name: 'login' }">Have an account?</router-link>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">
-        Create Account
-      </button>
+      <label id="box1-1" for="username" class="textclass">Username</label>
+      <input type="text" id="box1-2" class="formclass" placeholder="Username..." v-model="user.username" required autofocus/>
+      <label id="box2-1" for="email" class="textclass">Email</label>
+      <input type="text" id="box2-2" class="formclass" placeholder="Email..." v-model="user.email" required autofocus/>
+      <label id="box3-1" for="password" class="textclass">Password</label>
+      <input type="password" id="box3-2" class="formclass" placeholder="Password..." v-model="user.password" required/>
+      <input type="password" id="box3-3" class="formclass" placeholder="Confirm Password..." v-model="user.confirmPassword" required/>
+      <label id="end" class="textclass"></label>
+      <router-link id="box4-1" :to="{ name: 'login' }">Have an account?</router-link>
+      <button id="button" class="btn" type="submit">Create Account</button>
     </form>
   </div>
 </template>
@@ -101,4 +70,136 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+
+
+#register{
+  background-color: rgb(105, 172, 105);
+}
+
+.form-register{
+  display: grid;
+  grid-template-columns: 1fr 2fr 2fr 1fr;
+  /* grid-gap: 3px; */
+  grid-template-areas: 
+  ". header header ."
+  ". usename usename ."
+  ". usenamein usenamein ."
+  ". email email ."
+  ". emailin emailin ."
+  ". pass pass ."
+  ". passin passin ."
+  ". passcon passcon ."
+  ". formbottom formbottom ."
+  ". createbutton loginlink .";
+}
+
+.maintext{
+  font-family: Consolas, Arial, Helvetica;
+  /* border-radius: 25px; */
+  border-top-right-radius: 25px;
+  border-top-left-radius: 25px;
+  font-size: 50px;
+  text-align: center;
+  color: rgb(105, 172, 105);
+  font-weight: bolder;
+  line-height: 50px;
+  background-color: lightgray;
+  /* margin-bottom: 15px; */
+  margin-top: 10px;
+  padding-bottom: 25px;
+  padding-top: 15px;
+}
+
+.textclass{
+  font-family: Consolas, Arial, Helvetica;
+  padding-top: 15px;
+  padding-bottom: 3px;
+  padding-left: 4px;
+  font-size: 35px;
+  text-align: left;
+  color: rgb(105, 172, 105);
+  line-height: 30px;
+  background-color: lightgray;
+}
+
+#end{
+  font-family: Consolas, Arial, Helvetica;
+  border-bottom-right-radius: 25px;
+  border-bottom-left-radius: 25px;
+  padding-top: 35px;
+  padding-bottom: 3px;
+  padding-left: 4px;
+  font-size: 35px;
+  text-align: left;
+  color: rgb(105, 172, 105);
+  line-height: 30px;
+  background-color: lightgray;
+  grid-area: formbottom;
+}
+
+.formclass{
+  font-family: Consolas, Arial, Helvetica;
+  border-color: gray;
+  font-size: 28px;
+  color: rgb(13, 42, 13);
+  background-color: lightgray;
+  line-height: 20px;
+}
+
+#box4-1{
+  grid-area: loginlink;
+  text-align: right;
+  font-size: 25px;
+  bottom: 0%;
+  margin-top: 20px;
+
+}
+
+#button{
+  grid-area: createbutton;
+  background-color: lightgray;
+  font-family: Consolas, Arial, Helvetica;
+  color: rgb(105, 172, 105);
+  border: none;
+  border-radius: 10px;
+  padding: 10px 20px;
+  font-size: 20px;
+  line-height: 20px;
+  width: 50%;
+  margin-top: 20px;
+  margin-bottom: 100%;
+  transition-duration: .4s;
+}
+#button :hover{
+  background-color: white;
+  color: black;
+}
+
+#header{
+  grid-area: header;
+}
+
+#box1-1{
+  grid-area: usename;
+}
+#box1-2{
+  grid-area: usenamein;
+}
+#box2-1{
+  grid-area: email;
+}
+#box2-2{
+  grid-area: emailin;
+}
+#box3-1{
+  grid-area: pass;
+}
+#box3-2{
+  grid-area: passin;
+}
+#box3-3{
+  grid-area: passcon;
+}
+
+</style>
