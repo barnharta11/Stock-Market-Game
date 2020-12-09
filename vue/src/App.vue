5f7bad4491fa9892596e6f509b95eaa7fa9f88b6
David Perez <davidperez3611@gmail.com>
2020-12-09 11:34:56 -0500

    css fix

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,26 +1,60 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link id="homebutton" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp; &nbsp;
-      <router-link id="logout" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link id="homebutton" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp; &nbsp;|
+      <router-link id="logoutbutton" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
   </div>
 </template>
 
 <style>
-#app{
+#nav{
   background-color: black; 
   font-family: Consolas, Arial, Helvetica;
+  color: rgb(105, 172, 105);
+  font-family: Consolas, Arial, Helvetica;
+  font-size: 35px;
+
 }
 #homebutton:link{
+  background-color: black;
   color: rgb(105, 172, 105);
   font-family: Consolas, Arial, Helvetica;
   font-size: 35px;
   text-decoration: none;
 }
 
+#homebutton:visited{
+  color:rgb(105, 172, 105);
+  font-family: Consolas, Arial, Helvetica;
+  font-size: 35px;
+  text-decoration: none;
+}
+
 #homebutton:hover{
+  color: white;
+  font-family: Consolas, Arial, Helvetica;
+  font-size: 35px;
+  text-decoration: none;
+}
+
+#logoutbutton:link{
+  background-color: black;
+  color: rgb(105, 172, 105);
+  font-family: Consolas, Arial, Helvetica;
+  font-size: 35px;
+  text-decoration: none;
+}
+
+#logoutbutton:visited{
+  color:rgb(105, 172, 105);
+  font-family: Consolas, Arial, Helvetica;
+  font-size: 35px;
+  text-decoration: none;
+}
+
+#logoutbutton:hover{
   color: white;
   font-family: Consolas, Arial, Helvetica;
   font-size: 35px;
