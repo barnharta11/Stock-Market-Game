2b9a1567e9354ed4d6007c405a69c00e935d9965
Alicia Barnhart <aliciambarnhart@gmail.com>
2020-12-11 12:26:30 -0500

    tables for leaderboard and portfolio

--- vue/src/components/LeaderboardComponent.vue
+++ vue/src/components/LeaderboardComponent.vue
@@ -1,7 +1,29 @@
 <template>
-  <div>
-      
-  </div>
+  <div class="mainbackground">
+    <!-- <div id="" >
+        <h2>Leaderboard</h2>
+        <table id="gamelistcreatorname" class="smalltextclass">
+            <thead>
+                <tr>
+                    <th>Standing</th>
+                    <th>Username</th>
+                    <th>Net Worth</th>
+                    <th>Outstanding Cash</th>
+                    <th>Game Ends</th>
+                </tr>
+            </thead>
+            <tbody v-for="" v-bind:key="">
+                <tr>
+                    <td>{{- standing -}}</td>
+                    <td v-on:click='-see user portfolio -'>{{- username -}}</td>
+                    <td>{{- net worth -}}</td>
+                    <td>{{- outstanding cash -}}</td>
+                    <td>{{- game ends -}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div> -->
+</div> 
 </template>
 
 <script>
