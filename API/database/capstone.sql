0886d5abb2fd687611278b059e276b13e69ebceb
Keith Wier <wier.keith@gmail.com>
2020-12-11 16:05:38 -0500

    Updated database script

--- API/database/capstone.sql
+++ API/database/capstone.sql
@@ -46,7 +46,6 @@ CREATE TABLE games (
 Create Table portfolio (
 	portfolio_id int IDENTITY(1,1) NOT NULL,
 	user_game_id int NOT NULL,
-	balance money NOT NULL,
 	Constraint pk_portfolio_portfolio_id Primary Key (portfolio_id),
 );	
 
@@ -62,22 +61,22 @@ Create Table user_games (
 	Constraint ck_status_code check(status_code in (0,1))
 );
 
-Create Table stocks (
-	stock_id int IDENTITY(1,1) NOT NULL,
-	stock_ticker varchar(50) NOT NULL,
+Create Table assets (
+	asset_id int IDENTITY(1,1) NOT NULL,
+	symbol varchar(50) NOT NULL,
 	company_name varchar(50) NOT NULL,
 	current_price money NOT NULL,
-	Constraint pk_stocks_stock_id Primary Key (stock_id),
+	Constraint pk_assets_asset_id Primary Key (asset_id),
 );
 
-Create Table portfolio_stocks (
-	portfolio_stocks_id int IDENTITY(1,1) NOT NULL,
+Create Table portfolio_assets (
+	portfolio_assets_id int IDENTITY(1,1) NOT NULL,
 	portfolio_id int NOT NULL,
-	stock_id int NOT NULL,
+	asset_id int NOT NULL,
 	quantity_held int NOT NULL,
-	Constraint pk_portfolio_stocks_portfolio_stocks_id Primary Key (portfolio_stocks_id),
-	Constraint fk_portfolio_stocks_portfolio Foreign Key (portfolio_id) References portfolio(portfolio_id),
-	Constraint fk_portfolio_stocks_stock Foreign Key (stock_id) References stocks(stock_id),
+	Constraint pk_portfolio_assets_portfolio_assets_id Primary Key (portfolio_assets_id),
+	Constraint fk_portfolio_assets_portfolio Foreign Key (portfolio_id) References portfolio(portfolio_id),
+	Constraint fk_portfolio_assets_asset Foreign Key (asset_id) References assets(asset_id),
 );
 
 Create Table transaction_names (
@@ -93,10 +92,10 @@ Create Table transactions (
 	cost_basis money NOT NULL,
 	time datetime  NOT NULL,
 	user_game_id int NOT NULL,
-	stock_id int NOT NULL,
+	asset_id int NOT NULL,
 	Constraint pk_transaction_id Primary Key (transaction_id),
-	Constraint fk_transactions_stocks_user_games Foreign Key (user_game_id) References user_games(user_game_id),
-	Constraint fk_transactions_stocks_stocks Foreign Key (stock_id) References stocks(stock_id),
+	Constraint fk_transactions_assets_user_games Foreign Key (user_game_id) References user_games(user_game_id),
+	Constraint fk_transactions_assets_assets Foreign Key (asset_id) References assets(asset_id),
 	Constraint ck_trans_type_id check(trans_type_id in (0,1)),
 	Constraint fk_transactions_type_transaction_names Foreign Key (trans_type_id) References transaction_names(trans_id),
 );
@@ -105,8 +104,8 @@ Create Table transactions (
 INSERT INTO users (username, email, password_hash, salt, user_role) VALUES ('user', 'makesomethingup@someplace.com', 'Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
 INSERT INTO users (username, email, password_hash, salt, user_role) VALUES ('admin', 'totallynotgamblingatwork@hotmail.com','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
 
-INSERT INTO	game_status (status_id, status_name) VALUES (0, 'Pending'), (1, 'Accepted'), (2, 'Completed');
-
+INSERT INTO	game_status (status_id, status_name) VALUES (0, 'Pending'), (1, 'Accepted'), (2, 'Active'), (3, 'Completed');
+INSERT INTO assets (symbol, company_name, current_price) VALUES('USD', 'United States Dollar', 1)
 INSERT INTO	transaction_names (trans_id, trans_name) VALUES (0, 'Buy'), (1, 'Sell');
 
 INSERT INTO games (game_name, creator_id, start_date, end_date) VALUEs ('Wall St. Wolves', 1, 12/07/2020, 12/16/2020);
@@ -117,25 +116,35 @@ INSERT INTO games (game_name, creator_id, start_date, end_date) VALUEs ('Nothing
 INSERT INTO games (game_name, creator_id, start_date, end_date) VALUEs ('I Like Big Bucks', 2, 12/11/2020, 12/20/2020);
 
 INSERT INTO user_games (user_id, game_id, status_code) VALUEs (1, 1, 1);
-INSERT INTO portfolio (user_game_id ,balance) VALUEs (@@IDENTITY ,100000);
+INSERT INTO portfolio (user_game_id) VALUEs (@@IDENTITY);
+INSERT INTO portfolio_assets (portfolio_id, asset_id, quantity_held) VALUEs(@@IDENTITY, 1, 100000)
 INSERT INTO user_games (user_id, game_id, status_code) VALUEs (1, 2, 1);
-INSERT INTO portfolio (user_game_id ,balance) VALUEs (@@IDENTITY ,100000);
+INSERT INTO portfolio (user_game_id) VALUEs (@@IDENTITY);
+INSERT INTO portfolio_assets (portfolio_id, asset_id, quantity_held) VALUEs(@@IDENTITY, 1, 100000)
 INSERT INTO user_games (user_id, game_id, status_code) VALUEs (1, 3, 0);
-INSERT INTO portfolio (user_game_id ,balance) VALUEs (@@IDENTITY ,100000);
+INSERT INTO portfolio (user_game_id) VALUEs (@@IDENTITY);
+INSERT INTO portfolio_assets (portfolio_id, asset_id, quantity_held) VALUEs(@@IDENTITY, 1, 100000)
 INSERT INTO user_games (user_id, game_id, status_code) VALUEs (2, 3, 1);
-INSERT INTO portfolio (user_game_id ,balance) VALUEs (@@IDENTITY ,100000);
+INSERT INTO portfolio (user_game_id) VALUEs (@@IDENTITY);
+INSERT INTO portfolio_assets (portfolio_id, asset_id, quantity_held) VALUEs(@@IDENTITY, 1, 100000)
 INSERT INTO user_games (user_id, game_id, status_code) VALUEs (2, 4, 1);
-INSERT INTO portfolio (user_game_id ,balance) VALUEs (@@IDENTITY ,100000);
+INSERT INTO portfolio (user_game_id) VALUEs (@@IDENTITY);
+INSERT INTO portfolio_assets (portfolio_id, asset_id, quantity_held) VALUEs(@@IDENTITY, 1, 100000)
 INSERT INTO user_games (user_id, game_id, status_code) VALUEs (2, 1, 0);
-INSERT INTO portfolio (user_game_id ,balance) VALUEs (@@IDENTITY ,100000);
+INSERT INTO portfolio (user_game_id) VALUEs (@@IDENTITY);
+INSERT INTO portfolio_assets (portfolio_id, asset_id, quantity_held) VALUEs(@@IDENTITY, 1, 100000)
 INSERT INTO user_games (user_id, game_id, status_code) VALUEs (2, 5, 0);
-INSERT INTO portfolio (user_game_id ,balance) VALUEs (@@IDENTITY ,100000);
+INSERT INTO portfolio (user_game_id) VALUEs (@@IDENTITY);
+INSERT INTO portfolio_assets (portfolio_id, asset_id, quantity_held) VALUEs(@@IDENTITY, 1, 100000)
 INSERT INTO user_games (user_id, game_id, status_code) VALUEs (1, 5, 1);
-INSERT INTO portfolio (user_game_id ,balance) VALUEs (@@IDENTITY ,100000);
+INSERT INTO portfolio (user_game_id) VALUEs (@@IDENTITY);
+INSERT INTO portfolio_assets (portfolio_id, asset_id, quantity_held) VALUEs(@@IDENTITY, 1, 100000)
 INSERT INTO user_games (user_id, game_id, status_code) VALUEs (1, 6, 0);
-INSERT INTO portfolio (user_game_id ,balance) VALUEs (@@IDENTITY ,100000);
+INSERT INTO portfolio (user_game_id) VALUEs (@@IDENTITY);
+INSERT INTO portfolio_assets (portfolio_id, asset_id, quantity_held) VALUEs(@@IDENTITY, 1, 100000)
 INSERT INTO user_games (user_id, game_id, status_code) VALUEs (2, 6, 1);
-INSERT INTO portfolio (user_game_id ,balance) VALUEs (@@IDENTITY ,100000);
+INSERT INTO portfolio (user_game_id) VALUEs (@@IDENTITY);
+INSERT INTO portfolio_assets (portfolio_id, asset_id, quantity_held) VALUEs(@@IDENTITY, 1, 100000)
 
 --table alterations
 
