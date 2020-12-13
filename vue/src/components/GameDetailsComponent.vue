bf34b7d5fbf6bb6563281401bf0f3d2ef54a9baa
Keith Wier <wier.keith@gmail.com>
2020-12-13 16:48:06 -0500

    started styling details

--- vue/src/components/GameDetailsComponent.vue
+++ vue/src/components/GameDetailsComponent.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
-          <span>{{selectedGame.gameName}} | </span>
-          <span>{{selectedGame.creatorName}} | </span>
-          <span>{{selectedGame.startDate}} | </span>
-          <span>{{selectedGame.endDate}}</span>
-          
-          <!-- Display User's Portfolio (Pass current userID(from$store) to controller)-->
-              <!-- Display Balance and Net Balance on one line -->
-              <!-- Stocks in a table -->
-
-
-
-
+  <div id="gamedetailscontainer">
+          <table>
+            <thead id="gamedetailshead">
+              <th>Game Name</th>
+              <th>Game Creator</th>
+              <th>Start Date</th>
+              <th>End Date</th>
+            </thead>
+            <tbody id="gamedetailsbody">
+              <tr>
+                <td>{{selectedGame.gameName}}</td>
+                <td>{{selectedGame.creatorName}}</td>
+                <td>{{selectedGame.startDate}}</td>
+                <td>{{selectedGame.endDate}}</td>
+              </tr>
+            </tbody>
+          </table>
   </div>
 </template>
 
@@ -33,7 +37,19 @@ export default {
 </script>
 
 <style>
-
-
+#gamedetailscontainer{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-areas:
+  "head head head head"
+  "data data data data";
+  width: 100vw;
+}
+.gamedetailshead{
+  grid-area: head;
+}
+.gamedetailsbody{
+  grid-area: data;
+}
 
 </style>
